3bd8e91bbf8d0a3fd8dbadc8c15519be51f09f70
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-01-31 00:24:19 +0500

    Bug reported

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-expressions */
+import React from 'react'
 import './App.css';
 
-function App() {
+
+let swear = [
+'arse',
+'ass',
+'asshole',
+'bastard',
+'bitch',
+'bollocks',
+'brotherfucker',
+'bugger',
+'bullshit',
+'clit',
+'crap',
+'cunt',
+'damn',
+'effing',
+'fatherfucker',
+'frigger',
+'fuck'
+]
+
+const App = React.memo(() => {
+  let [count , setCount] = React.useState(0)
+  let [approval , setApproval] = React.useState(false)
+  let [text , setText] = React.useState('')
+
+
+  
+  const bogusCheck = (text) =>{
+    let textArray = text.split(' ')
+    console.log(textArray);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Profanity Checker</h1>
+      <p>Enter a sentence below and click the button below:</p>
+      <textarea cols="30" rows='10' value={text} onChange={e => setText(e.target.value) } />
+      <br />
+      <button onClick={bogusCheck(text)} >Profanity Check</button>
     </div>
+
   );
 }
-
+)
 export default App;
